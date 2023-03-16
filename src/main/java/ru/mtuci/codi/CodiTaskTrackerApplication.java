92e3aa9c9c22c16fa09d888874b7caa6a29ad027
Roman.Gaynullin <Roman.Gaynullin@rt.ru>
2023-03-16 22:26:29 +0300

    init spring boot

---
+++ src/main/java/ru/mtuci/codi/CodiTaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package ru.mtuci.codi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodiTaskTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodiTaskTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mtuci/codi/CodiTaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mtuci.codi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodiTaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
